c24283c2e980442b8de09b16bbdac77f00600a87
John Olgin <jolgin90@gmail.com>
2019-10-04 23:03:18 -0500

    Delete dbcon.js

--- Final Project - Database Interactions and UI/dbcon.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var pool = mysql.createPool({
-  host  : 'classmysql.engr.oregonstate.edu',
-  user  : 'cs290_olginj',
-  password: 'NWtx2048',
-  database: 'cs290_olginj'
-});
-
-module.exports.pool = pool;
